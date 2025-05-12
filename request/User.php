0969c29b7e908a70322b4baf07165724684dcd29
beppu-0509 <koki03210509@docomo.ne.jp>
2025-05-12 12:50:26 +0900

    Add files via upload

---
+++ request/User.php
@@ -0,0 +1,24 @@
+// User.php
+<?php
+class User {
+    private $name;
+    private $email;
+
+    public function __construct($name, $email) {
+        $this->name = $name;
+        $this->email = $email;
+    }
+
+    public function getName() {
+        echo "<p>aaa</p>";
+        echo $hen;
+        return $this->name;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+}
+?>
+
+これは外です
---
+++ request/sample.php
@@ -0,0 +1,21 @@
+//インポート
+//front->controller->view処理(標準出力処理
+//frontにリターンしてリクエスト属性データ格納view表示orシンプルcontroller->view
+//クラスとしてコントローラーから出力用関数を呼び出したら、全部htmlを埋め込まないといけなくなる
+//インポートともうひつとつで使い方違う気がする
+//クラスの読み込みと、そのまま取り込みみたいな？だから変数のスコープが違うかも だめだった。
+//処理からviewにどうやってデータを受け渡す？？？
+<?php
+// User.php をインポート
+require 'User.php'; // または include 'User.php';
+
+$hen = "test";
+
+// User クラスを使ってオブジェクトを作成
+$user = new User('Taro', 'taro@example.com');
+
+// メソッドを呼び出してデータを取得
+echo '名前: ' . $user->getName() . '<br>';
+echo 'メール: ' . $user->getEmail();
+?>
+表示処理
